b691f4a7ccc5fd1f93ec4ca62e738ef3ef17e354
EvanShapedDesktop <2020nave@gmail.com>
2025-05-31 16:07:39 -0400

    Add read only text field and state button to index page

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,16 +1,34 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function IndexPage() {
+    const [myDisplayVar, setMyDisplayVar] = useState("Default")
+
+    const handleFetchVal = () => {
+        setMyDisplayVar("new val")
+    }
+
     return (
         <Box
             classname='index-page-box'
+            display='flex'
             sx={{
+                p: 2,
                 borderWidth: 2,
                 borderStyle: 'dashed',
                 borderColor: 'black',
+                justifyContent: 'center',
             }}
         >
-            <Typography variant='h3'>Index Page</Typography>
+            <TextField
+                id="outlined-read-only-input"
+                label="Read Only"
+                value={myDisplayVar}
+                slotProps={{
+                    input: { readOnly: true, },
+                }}
+            />
+            <Button variant="contained" onClick={handleFetchVal}>Fetch val</Button>
         </Box>
     )
 }
